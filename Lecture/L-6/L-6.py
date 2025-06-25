6de49860114b018c16c52fc5808ea06f38c37942
vk-07vikram <vickypraj008@gmail.com>
2025-06-25 12:29:06 +0530

    Add files via upload

---
+++ Lecture/L-6/L-6.py
@@ -0,0 +1,36 @@
+# 1. Types of Loops in Python
+
+# for loop
+
+# for i in range(10):
+#   print("For Loop :" , i)
+
+# for i in range(1 , 10):
+#   print(i)
+
+# for i in range(1 , 10 , 3):
+#   print(i)
+
+# range (start , stop , step)
+
+# while loop
+
+# logic
+      # i+=1 [ i = 0 + 1 ] = 1
+      # i+=1 [ i = 1 + 1 ] = 2
+      # i+=1 [ i = 2 + 1 ] = 3
+      # i+=1 [ i = 3 + 1 ] = 4
+      # i+=1 [ i = 4 + 1 ] = 5
+i = 0 
+while i <= 10:
+  print("while loop:" , i)
+  i+=1
+
+# Nested Loop 
+
+# for i in range(1 , 5):
+#   for j in range(1 , 4):
+#     print(j)
+#   print()
+
+
